efb708475b5677e6e1f75de96c1cc5b2a8a75256
leelasowmyajan <leelasowmya.jandhyala@gmail.com>
2025-06-11 23:33:33 -0600

    Adding Album Class

--- src/main/java/com/leelasowmya/topalbums/domain/Album.java
+++ src/main/java/com/leelasowmya/topalbums/domain/Album.java
@@ -1,4 +1,31 @@
 package com.leelasowmya.topalbums.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@Table(name = "albums")
 public class Album {
+    @Id
+    @UuidGenerator
+    @Column(name = "id", unique = true, updatable = false)
+    private String id;
+    private String name;
+    private String artist;
+    private String releaseYear;
+    private String genre;
+    private String photoUrl;
 }
