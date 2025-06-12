c4e72b3f873eeeb5b0c0fd5cd549d7c6d1f409ad
leelasowmyajan <leelasowmya.jandhyala@gmail.com>
2025-06-12 01:03:51 -0600

    Adding Album Service logic

---
+++ src/main/java/com/leelasowmya/topalbums/constant/Constant.java
@@ -0,0 +1,5 @@
+package com.leelasowmya.topalbums.constant;
+
+public class Constant {
+    public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Documents/Projects/albumImages/";
+}
