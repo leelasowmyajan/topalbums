1aab60fcc6dce7738298dcef47e879176716808f
leelasowmyajan <leelasowmya.jandhyala@gmail.com>
2025-06-14 15:44:52 -0600

    Reapply .gitignore: remove accidentally committed ignored files

--- README.md
+++
@@ -1,5 +0,0 @@
-# Top Albums App 🎵
-
-A full-stack web application to manage and view music albums with photo upload support.
-
-Built using Spring Boot (Java) for the backend and React (JavaScript) for the frontend.
--- topalbums/src/main/java/com/leelasowmya/topalbums/constant/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.leelasowmya.topalbums.constant;
-
-public class Constant {
-    public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Documents/Projects/albumImages/";
-    public static final String PHOTO_PUBLIC_URL = "/albums/image/";
-}
