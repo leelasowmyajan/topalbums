6468e8222021296f8ab62992645c5c3b61d409a3
leelasowmyajan <leelasowmya.jandhyala@gmail.com>
2025-06-15 21:52:17 -0600

    Adding Cors related Configurations to allow requests from localhost

--- topalbums-backend/src/main/java/com/leelasowmya/topalbums/constant/Constant.java
+++ topalbums-backend/src/main/java/com/leelasowmya/topalbums/constant/Constant.java
@@ -3,4 +3,6 @@ package com.leelasowmya.topalbums.constant;
 public class Constant {
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Documents/Projects/albumImages/";
     public static final String PHOTO_PUBLIC_URL = "/albums/image/";
+
+    public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
