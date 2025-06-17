0db03b6b4c3320d5c26ef57b6faf94b4b4c122f3
leelasowmyajan <leelasowmya.jandhyala@gmail.com>
2025-06-16 21:06:24 -0600

    Adding Album card display and Modal display on click of Add New Album

--- topalbums-ui/src/App.js
+++ topalbums-ui/src/App.js
@@ -1,11 +1,105 @@
 import './App.css';
+import { useEffect, useState, useRef } from 'react';
+import { getAlbums } from './api/AlbumService';
+import Header from './components/Header';
+import AlbumList from './components/AlbumList'
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-function App() { 
+function App() {
+  const [data, setData] = useState({});  // State for storing albums data
+  const [currentPage, setCurrentPage] = useState(0);  // State for tracking current page
+  const modalRef = useRef();
+  const fileRef = useRef();
+  //to handle the form data
+  const [values, setValues] = useState({
+    name: '',
+    artist: '',
+    genre: '',
+    releaseYear: '',
+    albumUrl: '',
+  });
+  const [file, setFile] = useState(undefined);
+
+  const getAllAlbums = async (page = 0, size = 10) => {  // Function to fetch albums
+    try {
+      setCurrentPage(page);  // Set the current page
+      const { data } = await getAlbums(page, size);  // Fetch albums from the API
+      setData(data);  // Update the state with the fetched data
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onChangeFormData = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  // Open or close the modal
+  const toggleModal = show => show ? modalRef.current.showModal() : modalRef.current.close();
+
+  useEffect(() => {
+    getAllAlbums();  // Fetch albums when the component mounts
+  }, []);  // Empty array means this effect runs only once after the first render
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Header toggleModal={toggleModal} nbOfAlbums={data.totalElements} />
+      <main className='main'>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Navigate to={'/albums'} />} />
+            <Route path="/albums" element={<AlbumList data={data} currentPage={currentPage} getAllAlbums={getAllAlbums} />} />
+          </Routes>
+        </div>
+      </main>
+
+      {/* Modal */}
+      <dialog ref={modalRef} className="modal" id="modal">
+        <div className="modal__header">
+          <h3>New Album</h3>
+          <i onClick={() => toggleModal(false)} className="bi bi-x-lg"></i>
+        </div>
+        <div className="divider"></div>
+        <div className="modal__body">
+          <form>
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">Name</span>
+                <input type="text" value={values.name} onChange={onChangeFormData} name='name' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Artist</span>
+                <input type="text" value={values.artist} onChange={onChangeFormData} name='artist' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Genre</span>
+                <input type="text" value={values.genre} onChange={onChangeFormData} name='genre' />
+              </div>
+              <div className="input-box">
+                <span className="details">Release Year</span>
+                <input type="text" value={values.releaseYear} onChange={onChangeFormData} name='releaseYear' />
+              </div>
+
+              <div className="input-box">
+                <span className="details">Apple Music Link</span>
+                <input type="text" value={values.albumUrl} onChange={onChangeFormData} name='albumUrl' />
+              </div>
+              <div className="file-input">
+                <span className="details">Album Cover</span>
+                <input type="file" onChange={(event) => setFile(event.target.files[0])} ref={fileRef} name='albumPhoto' />
+              </div>
+            </div>
+            <div className="form_footer">
+              <button onClick={() => toggleModal(false)} type='button' className="btn btn-danger">Cancel</button>
+              <button type='submit' className="btn">Save</button>
+            </div>
+          </form>
+        </div>
+      </dialog>
+
+    </>
   );
 }
 
-export default App;
+export default App;  
--- topalbums-ui/src/components/Album.js
+++ topalbums-ui/src/components/Album.js
@@ -3,32 +3,32 @@ import { Link } from 'react-router-dom'
 
 // Album component - shows individual album card
 const Album = ({ album }) => {
-  return (
-    // Wrap the entire album card with a clickable link to its details page
-    <Link to={`/albums/${album.id}`} className="album__item">
-            <div className="album__header">
+    return (
+        // Wrap the entire album card with a clickable link to its details page
+        <div className="album__item">
+            <Link to={`/albums/${album.id}`} className="album__header">
                 <div className="album__image">
-                    <img src={album.photoUrl} alt={album.name}  />
+                    <img src={album.photoUrl} alt={album.name.substring(0, 1)} />
                 </div>
                 <div className="album__details">
                     {/* to stop overflow in the container, let's keep it to 15 chars */}
-                    <p className="album_name">{album.name.substring(0, 15)} </p>
+                    <p className="album_name">{album.name.substring(0, 20)} </p>
                     <p className="album_artist">{album.artist}</p>
                 </div>
-            </div> 
+            </Link>
             <div className="album__body">
                 <p><i className="bi bi-music-note-beamed"></i> {album.genre} </p>
                 <p><i className="bi bi-calendar3"></i> {album.releaseYear}</p>
                 <p><i className="bi bi-link-45deg"></i>{' '}
                     {/* target="_blank" = Opens link in a new browser tab
                     rel="noopener noreferrer" = Adds extra security  */}
-                    <a href={album.albumUrl} target="_blank" rel="noopener noreferrer"> 
+                    <a href={album.albumUrl} target="_blank" rel="noopener noreferrer">
                         Link to Apple Music
                     </a>
                 </p>
             </div>
-        </Link>
-  )
+        </div>
+    )
 }
 
 export default Album
